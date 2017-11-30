9b71bf4f8718497f3ccfcf40436bb7d55ca6e9ec
David Cortes <dcortes22@gmail.com>
2017-11-29 18:30:58 -0600

    Agregado Login y Registrar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout :layout_by_resource
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :cellphone, :email])
+  end
 
   private
 

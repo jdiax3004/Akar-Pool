74155259118265a2de242463f074fd320973cc78
Sergio Martinez <sergiomc1002@gmail.com>
2017-11-30 11:54:12 -0600

    Sign up Show y Create

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -69,6 +69,6 @@ class TripsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def trip_params
-      params.require(:trip).permit(:names, :seats, :car, :departure, :PlaceMarks)
+      params.require(:trip).permit(:names, :seats, :car, :departure, :placemarks)
     end
 end
